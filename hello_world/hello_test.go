65bda610a9098329098002aa66f52e9b704f084b
MLankaoui <marouanelankaoui66@gmail.com>
2025-05-12 11:44:04 +0100

    add more cases.

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,13 +4,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	// what you got from the function
-	got := Hello("Chris")
-	// what we expect
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		// what you got from the function
+		got := Hello("Chris")
+		// what we expect
+		want := "Hello, Chris"
 
-	// checking if what we got is what we expected
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		// checking if what we got is what we expected
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
