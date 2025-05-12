e1a37fb2a9634a864a4ad4575856defa5a87ff91
MLankaoui <marouanelankaoui66@gmail.com>
2025-05-12 11:28:07 +0100

    write some notes.

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -7,7 +7,7 @@ func TestHello(t *testing.T) {
 	// what you got from the function
 	got := Hello()
 	// what we expect
-	want := "Hello, world"
+	want := "Hello, worl"
 
 	// checking if what we got is what we expected
 	if got != want {
---
+++ hello_world/notes.md
@@ -0,0 +1,5 @@
+# Writing tests
+1. name the file ```xxx_test.go```
+2. the function must start with ```Test```
+3. test function takes one argument ```t *testing.T```
+4. to use ``` *testing.T``` type, we need to ```import "testing```
