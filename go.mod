54ee27e8f1fc4adfb4f13e6f7abab94082537b2e
MLankaoui <marouanelankaoui66@gmail.com>
2025-05-12 11:15:33 +0100

    initialize go mod and add tests for hello program.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tddgolang
+
+go 1.23.0
---
+++ hello_world/hello_test.go
@@ -0,0 +1,16 @@
+package main
+
+// importing the testing package
+import "testing"
+
+func TestHello(t *testing.T) {
+	// what you got from the function
+	got := Hello()
+	// what we expect
+	want := "Hello, world"
+
+	// checking if what we got is what we expected
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
